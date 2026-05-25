baf247c1943fd423ac1f611ce082018ad8d2a6da
barrosGuih <cunha.barros.guilherme@gmail.com>
2026-05-25 18:34:47 -0300

    att de teste

--- assets/Pages/inicio/Inicio.js
+++ assets/Pages/inicio/Inicio.js
@@ -112,7 +112,7 @@ const styles = StyleSheet.create({
   senhaesqueceu:{
     marginTop: 10,
     color: 'white',
-    textDecorationLine: 'underline'
+    textDecorationLine: 'underline',
 
   }
 });
